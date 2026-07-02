4479bebb11a7f67d4dc48bb78df013314749ef42
meli abdu <melitabdu@gmail.com>
2026-07-02 11:30:58 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -53,6 +53,7 @@ const allowedOrigins = [
   "http://localhost:5734",
   "http://localhost:3000",
   "https://eiasc-procurement-esmp.vercel.app",
+  "https://eiascbusinessprocurement.vercel.app/",
 ];
 
 app.use(
